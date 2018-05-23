6b99541960a4fa34fa6ee013de1ccbb9ce73c94d
Gopal <gopalakrishnanbe16@gmail.com>
2018-05-23 22:56:37 +0530

    - IsList Palindrome Stack method solution

--- CrackingTheCodingInterview/src/com/program/chapter2/Palindrome.java
+++ CrackingTheCodingInterview/src/com/program/chapter2/Palindrome.java
@@ -7,15 +7,17 @@ public class Palindrome {
 	public static void main(String[] args) {
 		SingleLinkedList list = new SingleLinkedList();
 		list.addNode(1);
-		list.addNode(2);
-		list.addNode(3);
-		list.addNode(4);
-		list.addNode(5);
-		list.addNode(6);
-		list.addNode(7);
+		list.addNode(1000000000);
+		list.addNode(-1000000000);
+		list.addNode(-1000000000);
+		list.addNode(1000000000);
+		list.addNode(1);
+		//list.addNode(1);
+		//list.addNode(7);
 		head = list.head;
 		list.printList();
 		usingStack();
+		System.out.println();
 	}
 	
 	public static void usingStack(){
@@ -23,18 +25,58 @@ public class Palindrome {
 		Node first = head;
 		Node second = head;
 		while(first.next!=null && first.next.next!=null){
-			
-			System.out.println(first.data+" fast ");
-			System.out.println(second.data+" slow ");
-			
+//			System.out.println(first.data+" fast ");
+//			System.out.println(second.data+" slow ");		
+			stack.push(second.data);
 			second = second.next;
 			first = first.next.next;
 			
 		}
-		System.out.println(first.data+" fast1 ");
-		System.out.println(second.data+" slow1 ");
+		//System.out.println(first.data+" fast1 ");
+		//System.out.println(second.data+" slow1 ");
+		if(first.next!=null){
+			stack.push(second.data);
+		}
+		
+		while(second.next!=null){
+			second = second.next;
+			if(stack.pop()!=second.data){
+				System.out.println("This is not Palindrome");
+				return;
+				
+			}
+		}
+		System.out.println("Palindrome");
+		stack.forEach(i -> System.out.println(i));
+	}	
+	boolean isListPalindrome(ListNode<Integer> l) {
+		Stack<Integer> stack = new Stack<Integer>();  
+		ListNode<Integer> first = l;
+		ListNode<Integer> second = l;
+		while(first.next!=null && first.next.next!=null){	
+			stack.push(second.value);
+			second = second.next;
+			first = first.next.next;
+			
+		}
+		if(first.next!=null){
+			stack.push(second.value);
+		}
+		
+		while(second.next!=null){
+			second = second.next;
+			
+			if(stack.pop()!=second.value){
+				return false;
+			}
+		}
+		return true;
 	}
-	
-	
-	
+	 class ListNode<T> {
+  ListNode(T x) {
+    value = x;
+  }
+  T value;
+  ListNode<T> next;
+}
 }

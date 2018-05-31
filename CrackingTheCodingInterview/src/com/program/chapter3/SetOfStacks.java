d52f2efe19b36c8fd974324ca1db7cc5baf4f06c
Gopal <gopalakrishnanbe16@gmail.com>
2018-05-31 07:18:13 +0530

    - Added Solution Set Of Stacks. - But some funcnalities still pending

--- CrackingTheCodingInterview/src/com/program/chapter3/SetOfStacks.java
+++ CrackingTheCodingInterview/src/com/program/chapter3/SetOfStacks.java
@@ -1,17 +1,55 @@
 package com.program.chapter3;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+
 
 public class SetOfStacks {
 	
 	int threshold;
 	List<StackClass> setOfStacks;
 	
+	public static void main(String[] args) {
+		SetOfStacks stacks = new SetOfStacks(3);
+		stacks.push(1);
+		stacks.push(2);
+		stacks.push(3);
+		
+		stacks.push(4);
+		stacks.push(5);
+		stacks.push(6);
+		
+	}
+	
 	public SetOfStacks(int capacity){
 		threshold = capacity;
+		setOfStacks = new ArrayList<SetOfStacks.StackClass>();
 		setOfStacks.add(new StackClass(threshold));
 	}
 	
+	public void push(int data){
+		System.out.println(setOfStacks.size());
+		StackClass stackClass = setOfStacks.get(setOfStacks.size()-1);
+		if(stackClass.isFull()){
+			setOfStacks.add(new StackClass(threshold));
+			stackClass = setOfStacks.get(setOfStacks.size()-1);
+		}
+		stackClass.push(data);
+	}
+	//TODO when you removed last element from on stack. Make sure to delete that stack from list  
+	public int pop(){
+		StackClass stackClass = setOfStacks.get(setOfStacks.size()-1);
+		int data = 0;
+		try {
+			data = stackClass.pop();
+		} catch (Exception e) {
+			System.out.println();
+		}
+		return data;
+	}
 	
 	private static class StackClass{
 		
@@ -24,10 +62,12 @@ public class SetOfStacks {
 			this.array = new int[capacity];
 			this.top = -1;
 		}
-		public void push(int data) throws Exception{
+		public void push(int data){
 			if(isFull()){
-				throw new Exception("Stack is Full");
+				System.out.println("It is Full");
+				return;
 			}
+			System.out.println(data+" is inserted in array "+this);
 			top++;
 			array[top] = data;
 		}
@@ -47,7 +87,7 @@ public class SetOfStacks {
 			return false;
 		}
 		public boolean isFull(){
-			if(top>=capacity){
+			if(top==capacity-1){
 				return true;
 			}
 			return false;

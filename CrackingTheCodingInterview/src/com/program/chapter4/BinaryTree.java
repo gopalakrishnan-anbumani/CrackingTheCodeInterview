be3cbd62f12d37d71de33bea582b2f45e3d2e92e
Gopal <gopalakrishnanbe16@gmail.com>
2018-06-05 23:17:51 +0530

    - Created Binary Tree Class

--- CrackingTheCodingInterview/src/com/program/chapter4/BinaryTree.java
+++ CrackingTheCodingInterview/src/com/program/chapter4/BinaryTree.java
@@ -9,8 +9,36 @@ public class BinaryTree {
 		root.left = new Node(2);
 		root.right = new Node(3);
 		
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		DFS(root);
 	}
 
+	public static void DFS(Node node){
+		
+		int i=0;
+		if(i==0){
+			System.out.print(" ");
+		}
+		if(node.left!=null){
+			DFS(node.left);
+			System.out.println(node.left.data+" left ");
+		}
+		//if(node!=null){
+			//DFS(node);
+			//System.out.println(node.data+" cent ");
+		//}
+		
+		if(node.right!=null){
+			DFS(node.right);
+			System.out.println(node.right.data+" ri8 ");
+		}
+		
+		
+	}
 	
 	// Tree Node
 	private static class Node{
